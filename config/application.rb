4802ab3edf0f6825bddec43740d0a1927076e597
Asilver-jpg <asilver7493@gmail.com>
2020-06-10 18:06:03 -0400

    fix cors

--- config/application.rb
+++ config/application.rb
@@ -28,12 +28,7 @@ module FaceBackendApi
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-          origins '*'
-          resource '*', headers: :any, methods: [:get, :post]
-      end
-    end
+ 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
